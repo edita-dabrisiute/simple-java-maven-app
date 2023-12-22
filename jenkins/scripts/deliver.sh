f313fa078ee7e8582727bb0dfed603bb823154c4
edita-dabrisiute <131198011+edita-dabrisiute@users.noreply.github.com>
2023-12-22 13:51:57 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -13,12 +13,14 @@ echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
+echo 'Name $NAME'
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
+echo 'VERSION $VERSION'
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
